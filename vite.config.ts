da82b73a987f083b554f95d06dc81153328afa12
Ialbertine <a.ingabire1@alustudent.com>
2025-11-03 15:18:49 +0200

    fix: issues of PWA

--- vite.config.ts
+++ vite.config.ts
@@ -30,22 +30,28 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "herhaven.svg",
-            sizes: "any",
-            type: "image/svg+xml",
-            purpose: "any maskable",
-          },
-          {
             src: "/icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "any",
+          },
+          {
+            src: "/icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
           },
           {
             src: "/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "any",
+          },
+          {
+            src: "/icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
         shortcuts: [
